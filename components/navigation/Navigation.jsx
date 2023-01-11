371f25bdd5e9af579d55ddfc736808a53741c53a
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-11 17:41:47 +0100

    finished styles

--- components/navigation/Navigation.jsx
+++ components/navigation/Navigation.jsx
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import styles from "./navigation.module.css";
 import { MdClose } from "react-icons/md";
 import { FiMenu } from "react-icons/fi";
-import Button from "../../components/Button/Button";
-import Logo from "../../assets/Logo/Logo";
+import { AiOutlineCar } from "react-icons/ai";
+import Image from "next/image";
 
 function Navigation() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <div className={styles.container}>
-      <nav>
+      <nav className={styles.nav}>
         <div className={styles.navigation}>
-          <Logo className={styles.logo} />
+          <AiOutlineCar className={styles.burger_logo} />
           <div
             className={styles.burger_menu}
             onClick={() => setIsMenuOpen(!isMenuOpen)}
@@ -26,29 +26,10 @@ function Navigation() {
         </div>
         <div className={`${styles.menu} ${isMenuOpen ? styles.open : ""}`}>
           <ul className={styles.link_list}>
-            <li>
-              <a
-                href="#about"
-                className={styles.links}
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
-              >
-                <span className={styles.number}>01.</span>About
-              </a>
-            </li>
-            <li>
-              <button className="button"> LogOut</button>
-            </li>
-            <li>
-              <a
-                href="#work"
-                className={styles.links}
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
-              >
-                <span className={styles.number}>03.</span> Work Experience
-              </a>
-            </li>
+            <li>Account</li>
+            <li>LogOut</li>
+            <li>LogIn</li>
           </ul>
-          <Button className={styles.button} button_text="Get my Resume" />
         </div>
       </nav>
     </div>
