12d87965bc4d7e58e1fd3bb9e29fc5835cf9ff1f
MarianneH <marianne.helbig@googlemail.com>
2023-01-11 20:13:40 +0100

    added files for user profile

---
+++ pages/user/[id].js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./UserProfile.module.css";
+
+function UserProfile() {
+  return <div className={styles.container}></div>;
+}
+
+export default UserProfile;
