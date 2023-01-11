0162eb6b845c34d2d89f75adf045727118317981
MarianneH <marianne.helbig@googlemail.com>
2023-01-11 20:28:40 +0100

    add axios to dependencies

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styles from "./UserProfile.module.css";
 import { useRouter } from "next/router";
 
