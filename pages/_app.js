10edb8bf80e91dddc9dc5e7417c112eeff271271
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-11 17:21:37 +0100

    added Navigation globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '../styles/globals.css'
+import Navigation from "../components/navigation/Navigation";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navigation customer_logged_out />
+      <Component {...pageProps} />
+    </>
+  );
 }
