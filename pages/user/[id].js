98369186e4b22c00f429a0e6e8cb596211f91b7f
MarianneH <marianne.helbig@googlemail.com>
2023-01-11 21:19:32 +0100

    add asset folder

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styles from "./UserProfile.module.css";
 import { useRouter } from "next/router";
+import Image from "next/image";
 import axios from "axios";
 import { FiEdit2 } from "react-icons/fi";
 
@@ -10,15 +11,15 @@ function UserProfile() {
     name: "HansWurst Zimmerfrau",
     info: "I'm a star!",
     user_email: "wunderbar@schönesleben.com",
-    is_customer: true,
+    is_customer: false,
     is_company: false,
-    is_admin: false,
+    is_admin: true,
     company_id: null,
     current_rental: {
       id: 1,
       vehicle_name: "Opel Astra",
       vehicle_image:
-        "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2FyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60",
+        "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2FyfGVufDB8fDB8fA%3D%3D",
       plate_no: "B-OS 5555",
       company_name: "schöne Autos",
       pick_up_venue: "Berlinale Palast",
@@ -39,16 +40,27 @@ function UserProfile() {
 
   return (
     <div className={styles.container}>
-      <h1>User Profile</h1>
+      <h1>{userData.is_admin ? "Admin" : "User"} Profile</h1>
       <div className={styles.profile_info}>
         <div>
           <div>{userData.name}</div>
           <div>{userData.user_email}</div>
           <div>{userData.info}</div>
         </div>
-        <FiEdit2 />
+        <FiEdit2 className={styles.icon} />
       </div>
-      <div className={styles.current_booking}></div>
+      <div className={styles.current_booking}>
+        <h2>Your next rental</h2>
+        <div className={styles.rental_details}>
+          <Image
+            src={userData.current_rental.vehicle_image}
+            fill
+            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+          />
+        </div>
+      </div>
     </div>
   );
 }
