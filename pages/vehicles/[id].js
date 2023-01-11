2cbb32f2572adf6f79a085f01d4b25db6b2219a2
Nevin Alisheva <nevin.alisheva@gmail.com>
2023-01-12 00:58:08 +0100

    styling [id]

--- pages/vehicles/[id].js
+++ pages/vehicles/[id].js
@@ -1,28 +1,71 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import useSWR from 'swr';
+import styles from './[id].module.css'
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
+const fetcher = async () => {
+    const response = await fetch('url')
+    const data = await response.json()
+    return data
+}
 
-const Details = () => {
-        const { data, error } = useSWR('/api/profile-data', fetcher)
+const VehicleById = () => {
+    let data = 
+      {
+        vehicle_id: 1,
+        vehicle_name: "Ford Galaxie 500",
+        vehicle_desc: "lalala",
+        vehicle_brand: "Ford",
+        vehicle_model: "peagot 206",
+        mileage: 333999,
+        availability: false,
+        plate_no: "plate_no",
+        location_id: 3,
+        image: null,
+        company_id: 4,
+      };
 
-    if (error) return <div>Failed to load</div>
-    if (!data) return <div>Loading...</div>
+    // const router = useRouter();
+    // const {id} = router.query;
+
+    // const { data, error } = useSWR(id ? `/api/vehicles/${id}` : null, fetcher)
+
+    // if (error) return <div>Failed to load</div>
+    // if (!data) return <div>Loading...</div>
 
   return (
-    <div className="container">
-      <Image
-        src={data.image ? data.image : "/Image_not_available.png"}
-        alt="vehicle-image"
-        width={400}
-        height={400}
-      />
-      <div className="info">
-        <h1>{data.vehicle_name}</h1>
-
+    <div>
+      <div className={styles.container}>
+        <div className={styles.img}>
+        <Image
+          src={data.image ? data.image : "/Image_not_available.png"}
+          alt="vehicle-image"
+          width={400}
+          height={400}
+          
+        />
         </div>
+        <div className={styles.desc}>
+          <div className={styles.info}>
+            <h1>{data.vehicle_name}</h1>
+            <div className={styles.details}
+            {data.vehicle_brand && <p>{data.vehicle_brand}</p>}
+            {data.vehicle_model && <p>{data.vehicle_model}</p>}
+            {data.vehicle_desc && <p>{data.vehicle_desc}</p>}
+            {data.plate_no && <p>{data.plate_no}</p>}
+            {data.mileage && <p>{data.mileage}</p>}
+            </div>
+          </div>
+          {data.availability ? (
+            <button className="{styles.button}">Book now</button>
+          ) : (
+            <button className="{styles.booked-button}">Booked out</button>
+          )}
+        </div>
+       
+      </div>
     </div>
   );
 };
 
-export default Details;
+export default VehicleById;
--- pages/vehicles/index.js
+++ pages/vehicles/index.js
@@ -1,13 +1,18 @@
 import Image from "next/image";
 // import useSWR from "swr";
 
-// const fetcher = (...args) => fetch(...args).then((res) => res.json())
+const fetcher = async () => {
+  const response = await fetch("url");
+  const data = await response.json();
+  return data;
+};
 
 const Details = () => {
-  //       const { data, error } = useSWR('/api/profile-data', fetcher)
+        const { data, error } = useSWR('/api/vehicles', fetcher)
 
-  //   if (error) return <div>Failed to load</div>
-  //   if (!data) return <div>Loading...</div>
+    if (error) return <div>Failed to load</div>
+    if (!data) return <div>Loading...</div>
+    //map.data
 
   return (
     <div className="container">
@@ -24,3 +29,37 @@ const Details = () => {
 };
 
 export default Details;
+
+
+// import Image from "next/image";
+// import useSWR from 'swr';
+
+// const fetcher = async () => {
+//     const response = await fetch('url')
+//     const data = await response.json()
+//     return data
+// }
+
+// const Details = () => {
+//     const { data, error } = useSWR('/api/vehicle-data', fetcher)
+
+//     if (error) return <div>Failed to load</div>
+//     if (!data) return <div>Loading...</div>
+
+//   return (
+//     <div className="container">
+//       <Image
+//         src={data.image ? data.image : "/Image_not_available.png"}
+//         alt="vehicle-image"
+//         width={400}
+//         height={400}
+//       />
+//       <div className="info">
+//         <h1>{data.vehicle_name}</h1>
+
+//         </div>
+//     </div>
+//   );
+// };
+
+// export default Details;
