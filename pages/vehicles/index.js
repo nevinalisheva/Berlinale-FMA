cc26c28c7b190b76bcb170c0711af0cf592b40b9
orianemgn <mgnoriane@gmail.com>
2023-01-13 09:05:35 +0100

    add button and form add vehicles

--- pages/vehicles/index.js
+++ pages/vehicles/index.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import useSWR from 'swr';
+import useSWR from "swr";
 import React, { useState } from "react";
 import VehicleCard from "../../components/vehicleCard/VehicleCard";
 import styles from "./index.module.css";
-
+import AddVehicule from "../../components/addVehicule/AddVehicule";
 
 const fetcher = async () => {
   const response = await fetch("http://localhost:3000/api/vehicles/");
@@ -12,17 +12,25 @@ const fetcher = async () => {
 };
 const fetcherLoc = async () => {
   const response = await fetch("http://localhost:3000/api/locations");
-  const data= await response.json();
+  const data = await response.json();
   return data;
 };
 
 const VehicleList = () => {
   const [selected, setSelected] = useState("");
-  const { data:carList, error } = useSWR("/api/vehicles", fetcher);
+  const { data: carList, error } = useSWR("/api/vehicles", fetcher);
   console.log(carList);
 
-  const { data:locations, error:errorLoc } = useSWR("/api/locations", fetcherLoc);
+  const { data: locations, error: errorLoc } = useSWR(
+    "/api/locations",
+    fetcherLoc
+  );
   console.log(locations);
+  const [addCompontentVehicle, setAddComponentVehicle] = useState(false);
+
+  const handleAddVehicule = () => {
+    setAddComponentVehicle(!addCompontentVehicle);
+  };
 
   if (error) return <div>Failed to load</div>;
   if (!carList) return <div>Loading...</div>;
@@ -30,11 +38,16 @@ const VehicleList = () => {
   if (!locations) return <div>Loading...</div>;
 
   console.log("SELECTED", selected);
- 
 
   return (
     <div className={styles.container}>
       <h1>Cars available</h1>
+      {/* show only if user is a compagny */}
+      <button onClick={handleAddVehicule}>Add vehicle</button>
+      {/* show if Add vehiicule is clicked */}
+      {addCompontentVehicle && <AddVehicule />}
+
+      {/* show form only if user is a client */}
       <form>
         <label htmlFor="location-selec">
           Filter by location {""}
@@ -67,4 +80,3 @@ const VehicleList = () => {
 };
 
 export default VehicleList;
-
